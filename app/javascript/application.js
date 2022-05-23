79a379524bddf9e98cce869461bd77bf5b227f9d
Ogada Moses <ogadamoses59@gmail.com>
2022-05-23 16:36:15 +0300

    Complete login and Sign Up

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "channels"
+import "jquery"
+import "bootstrap"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
