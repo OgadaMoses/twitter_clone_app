6b690c967c81412718a6eeaed6650ca9ada31137
Ogada Moses <ogadamoses59@gmail.com>
2022-06-18 10:20:38 +0300

    Modify session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,7 +38,6 @@ module SessionsHelper
     if(user_id = session[:user_id])
       user = User.find_by(id: user_id)
       @current_user ||= user if session[:session_token] == user.session_token
-      @current_user ||= user id session[:session_token] == user.session_token
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(:remember, cookies[:remember_token])
