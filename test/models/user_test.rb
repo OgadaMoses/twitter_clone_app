9807f37a65bb5114911ff9f3727f2abba0e09eb4
Ogada Moses <ogadamoses59@gmail.com>
2022-06-20 10:08:44 +0300

    Begin Microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -75,6 +75,14 @@
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+
+  test "associated micropost should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Hello World")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
 
 
