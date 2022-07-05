33307b94dbc80e011e7f9ca561ea8dc884cf895a
Ogada Moses <ogadamoses59@gmail.com>
2022-07-05 19:55:28 +0300

    Finish Twitter Clone App

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,21 @@ class UsersController < ApplicationController
   else
     render 'edit'
   end
+
+  def following 
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+    end
+
 end
 
   private 
